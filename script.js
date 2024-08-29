c35d52bc76c9d825f7cb22752cd9f681a6a94287
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-29 08:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const buttons = document.querySelectorAll('.btn');
-const buttons = document.querySelectorAll('.stop');
+const stopButton = document.querySelectorAll('.stop');
 
 let currentSound = null;
 
